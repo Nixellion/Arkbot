d4ce717de9e276ef14da6b19a8221c0cffac751a
Nixellion <nixesvfx@gmail.com>
2020-05-03 22:22:21 +0300

    Added backup

---
+++ ark_backup.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+from ark_manager import *
+
+from paths import *
+
+
+def back_up(folder=None):
+    if folder:
+        backup_dir = os.path.join(BACKUPS_DIR, folder)
+    else:
+        backup_dir = BACKUPS_DIR
+    log.info(f"Backing up server data into {backup_dir}")
+
+    for filepath in BACKUP_FILES:
+        filename = os.path.basename(filepath)
+        dest = os.path.join(backup_dir, filename)
+        log.debug(f"Copying {filepath} to {dest}")
+        shutil.copy(filepath, dest)
+
+    log.info("Backup complete.")
+
+if __name__ == "__main__":
+    from locks import Lock
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Restart ark server.')
+    parser.add_argument("--message", dest='message', default=None,
+                        help="Message to add to broadcast, usually reason for restart.")
+    args = parser.parse_args()
+
+    if args.message:
+        args.message = args.message.strip()
+        if not args.message.endswith("."):
+            args.message = args.message + ". "
+        else:
+            args.message = args.message + " "
+
+    lock = Lock()
+
+    if lock.locked:
+        log.debug("Another script already running, exit...")
+        sys.exit()
--- paths.py
+++ paths.py
@@ -4,6 +4,8 @@ ARK_STEAMID = "346110"
 
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 
+CONFIG_FILE_PATH = os.path.join(APP_DIR, "config", "config.yaml")
+
 HOME_DIR = r"/home/arkserver"
 
 ARK_SERVER_DIR = os.path.join(HOME_DIR, "serverfiles")
@@ -15,12 +17,25 @@ ARK_MODS_DIR = os.path.join(ARK_SERVER_DIR, "ShooterGame", "Content", "Mods" )
 STEAMCMD = r"/home/arkserver/steamcmd/steamcmd.sh"
 
 STEAM_MODS_DIR = os.path.join("/home/arkserver/.local/share/Steam/steamapps/workshop/content", ARK_STEAMID)
+
 CONFIGS_DIR = r"/home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer"
+
 GAMEUSERSETTINGS = os.path.join(CONFIGS_DIR, "GameUserSettings.ini")
 
 DOWNLOADS_DIR = os.path.join(APP_DIR, "downloads")
 
+LGSM_CONFIG_DIR = os.path.join(HOME_DIR, "lgsm", "config-lgsm", "arkserver")
 
 STANDARD_MODS = ["111111111", "Ragnarok", "TheCenter", "Valguero"]
 
+BACKUPS_DIR = os.path.join(APP_DIR, "backups")
 
+BACKUP_FILES = [
+    CONFIG_FILE_PATH,
+    os.path.join(CONFIGS_DIR, "Game.ini"),
+    os.path.join(CONFIGS_DIR, "GameUserSettings.ini"),
+]
+
+
+for filename in ["_default.cfg", "arkserver.cfg",  "common.cfg"]:
+    backup_files.append(os.path.join(LGSM_CONFIG_DIR, filename))
