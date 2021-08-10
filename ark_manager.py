7a72bc240d26090d08c9eb7b339f4459f0e31a2a
Michael Davydov <nixesvfx@gmail.com>
2021-08-10 10:45:53 +0300

    Fix mods permissions add savedir as well

--- ark_manager.py
+++ ark_manager.py
@@ -294,6 +294,7 @@ def fix_permissions(path):
 def fix_mods_permissions(*args, **kwargs):
     fix_permissions(ARK_MODS_DIR)
     fix_permissions(ARK_CONFIGS_DIR)
+    fix_permissions(ARK_SAVED_DIR)
 
 
 import zipfile
